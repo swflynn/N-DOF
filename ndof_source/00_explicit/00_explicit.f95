041a68a78c539368a1f4ea47c437225ec2811866
swflynn <swflynn@uci.edu>
2017-02-20 17:48:40 -0800

    Calculate <0|G|0> using trapezoid approx.

--- ndof_source/00_explicit/00_explicit.f95
+++ ndof_source/00_explicit/00_explicit.f95
@@ -2,19 +2,14 @@ PROGRAM basis00
 
   IMPLICIT NONE
   REAL, PARAMETER :: force_const = 1.0, reduc_mass = 1.0, hbar = 1.0
-  REAL, PARAMETER :: upper_limit=3.0
+  REAL, PARAMETER :: upper_limit=10.0
   REAL :: a, coef, increment
   REAL, PARAMETER :: pi = 4*atan(1.0)
-  INTEGER, PARAMETER :: step_size=10
+  INTEGER, PARAMETER :: step_size=1000
   INTEGER :: i
 
   a = ((force_const*reduc_mass)**0.5)/hbar
   coef= (1/pi)* (a/2)**0.5
-
-  DO i = 0,step_size
-     increment = upper_limit*i/step_size
-     WRITE (*,*) increment, integrand(increment)
-  END DO
 
   CALL trapezoid_integration(step_size,upper_limit)
 
