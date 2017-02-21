def78fe9d0a744cc6d79b609371617b291ce7ec0
swflynn <swflynn@uci.edu>
2017-02-20 17:24:51 -0800

    Calculate <0|G(x)|0> Using Trapezoid approximation

--- ndof_source/00_explicit/00_explicit.f95
+++ ndof_source/00_explicit/00_explicit.f95
@@ -3,7 +3,7 @@ PROGRAM basis00
   IMPLICIT NONE
   REAL, PARAMETER :: force_const = 1.0, reduc_mass = 1.0, hbar = 1.0
   REAL, PARAMETER :: upper_limit=3.0
-  REAL :: a, coef, u
+  REAL :: a, coef, increment
   REAL, PARAMETER :: pi = 4*atan(1.0)
   INTEGER, PARAMETER :: step_size=10
   INTEGER :: i
@@ -12,13 +12,34 @@ PROGRAM basis00
   coef= (1/pi)* (a/2)**0.5
 
   DO i = 0,step_size
-     u = upper_limit*i/step_size
-     WRITE (*,*) u, integrand(u)
+     increment = upper_limit*i/step_size
+     WRITE (*,*) increment, integrand(increment)
   END DO
 
-  STOP
+  CALL trapezoid_integration(step_size,upper_limit)
 
   CONTAINS
+
+    SUBROUTINE trapezoid_integration(step_size,upper_limit)
+      IMPLICIT NONE
+      INTEGER :: step_size, i
+      REAL :: upper_limit, integral, increment, h
+
+      integral = 0.0
+
+      do i=0,step_size
+        increment = (upper_limit*i) / step_size
+
+        IF ((i.EQ.0) .OR. (i.EQ.step_size)) THEN
+          integral = integral+(2.0*integrand(increment))
+        END IF
+      ENd DO
+
+      h = upper_limit/step_size
+      integral = (h/2.0)*integral*coef
+
+      WRITE (*,*) 'Trapezoid Integration give : ', integral
+    END SUBROUTINE trapezoid_integration
 
     FUNCTION integrand(x) RESULT (values)
       IMPLICIT NONE
