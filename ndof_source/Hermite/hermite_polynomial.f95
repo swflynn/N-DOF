d32e8426d93ba13caa12575239d8ea5cdcc850d9
swflynn <swflynn@uci.edu>
2017-02-21 19:52:32 -0800

    Update Coefficient Calculation

--- ndof_source/Hermite/hermite_polynomial.f95
+++ ndof_source/Hermite/hermite_polynomial.f95
@@ -7,7 +7,7 @@ REAL*8 :: A(0:10)
 REAL*8 :: B(0:10,0:10)
 INTEGER :: degree,k
 
-   degree = 6
+   degree = 4
      WRITE(*,25) degree
 
      CALL Hermite_Coeff(degree,A,B)
@@ -15,34 +15,31 @@ INTEGER :: degree,k
      DO k = 0, degree
        WRITE(*,50) k, A(k)  
      END DO 
-     PRINT *,' '
-   print *,' '
 
    stop
 
 !Pretty formatting for writing coefficients to screen not necessary
-25 format('Coefficients for Hermite Polynomial ',i2/)
-50 format('   A(',i2,') = ',f10.0)
+25 format('Coefficients for Hermite Polynomial',i2/)
+50 format('A(',i2,') = ',f10.0)
 
 END PROGRAM HERMITE
 
 SUBROUTINE Hermite_Coeff(degree,A,B)  
   INTEGER i,j,degree
   REAL*8  A(0:10), B(0:10,0:10)
-  !Establish l0 and l1 coefficients
-  B(0,0)=1.d0 ; B(1,0)=0.d0 ; B(1,1)=2.d0
+  B(0,0)=1.D0 ; B(1,0)=0.D0 ; B(1,1)=2.D0
   !Return if order is less than two
   IF (degree>1) THEN 
     DO i = 2, degree
-      B(i,0)=-2.d0*(i-1)*B(i-2,0)
-      do j = 1, i
-        !Basic recursion relation
-        B(i,j)=2.d0*B(i-1,j-1)-2.d0*(i-1)*B(i-2,j)
-      end do
-    end do
-    do i = 0, degree
+      B(i,0)=-2.D0*(i-1)*B(i-2,0)
+      DO j = 1, i
+!Basic recursion relation
+        B(i,j)=2.D0*B(i-1,j-1)-2.D0*(i-1)*B(i-2,j)
+        END dO 
+        END DO 
+      DO i = 0, degree
       A(i)=B(degree,i)
-    end do
-  end if
-  return
+      END DO
+      END IF
+  RETURN 
 END SUBROUTINE Hermite_Coeff
