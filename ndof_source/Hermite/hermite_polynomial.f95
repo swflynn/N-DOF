dcc3285fd2048522e331a577849f54155869f143
swflynn <swflynn@uci.edu>
2017-02-21 20:10:17 -0800

    Hermite including degree 0, 1

--- ndof_source/Hermite/hermite_polynomial.f95
+++ ndof_source/Hermite/hermite_polynomial.f95
@@ -1,13 +1,13 @@
-! Need to generate the hermite polynomials for the harmonic oscillator wavefunction
-! Have found code to generate their coeficient, we then know they are jsut powers of x based on how many integers there are (prints 0 if no power)
+! Code to generate Hermite Polynomiali coeficients, only for polynomial = 10
 
 PROGRAM Hermite
+  IMPLICIT NONE
 
-REAL*8 :: A(0:10)
-REAL*8 :: B(0:10,0:10)
-INTEGER :: degree,k
+  REAL*8 :: A(0:10)
+  REAL*8 :: B(0:10,0:10)
+  INTEGER :: degree,k
 
-   degree = 4
+   degree = 5
      WRITE(*,25) degree
 
      CALL Hermite_Coeff(degree,A,B)
@@ -25,21 +25,35 @@ INTEGER :: degree,k
 END PROGRAM HERMITE
 
 SUBROUTINE Hermite_Coeff(degree,A,B)  
+  IMPLICIT NONE
   INTEGER i,j,degree
   REAL*8  A(0:10), B(0:10,0:10)
   B(0,0)=1.D0 ; B(1,0)=0.D0 ; B(1,1)=2.D0
-  !Return if order is less than two
-  IF (degree>1) THEN 
+
+! my comments
+ IF (degree == 0) THEN
+   A(0) = 1
+   RETURN 
+
+ ELSE IF (degree == 1) THEN
+   A(0) = 0
+   A(1) = 2
+   RETURN 
+
+  ELSE IF (degree>1) THEN 
     DO i = 2, degree
       B(i,0)=-2.D0*(i-1)*B(i-2,0)
       DO j = 1, i
+
 !Basic recursion relation
+
         B(i,j)=2.D0*B(i-1,j-1)-2.D0*(i-1)*B(i-2,j)
-        END dO 
-        END DO 
-      DO i = 0, degree
+      END dO 
+    END DO 
+    DO i = 0, degree
       A(i)=B(degree,i)
-      END DO
-      END IF
+    END DO
+  END IF
   RETURN 
+
 END SUBROUTINE Hermite_Coeff
