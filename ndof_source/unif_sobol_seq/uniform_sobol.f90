f287b0fbf541ad9e05eb72857ffa93d662d2a217
swflynn <swflynn@uci.edu>
2017-03-08 18:15:23 -0800

    Calculate MC integral using Sobol Seq (uniform)

--- ndof_source/unif_sobol_seq/uniform_sobol.f90
+++ ndof_source/unif_sobol_seq/uniform_sobol.f90
@@ -1,19 +1,49 @@
 ! Quick code to call sobol.f90 and generate sobol points
-!This makes a uniform dist. We want normal so use the scp_lmon module, not just sobol
+! Once we generate the points we integrate using MC
 
 PROGRAM unif_sobol_points
       USE  sobol
       IMPLICIT NONE
 
       INTEGER (kind=8) :: m, n, skip
-      Double Precision, allocatable :: r(:,:)
+      Double Precision, allocatable :: r(:,:), q(:,:)
+      REAL :: end_val, start_val, int_range, integral
+      INTEGER :: i
+      REAL*8 :: f
 
       m = 1                   !spatial dimension
-      n = 100                  !number of points to generate
+      n = 10000               !number of points to generate
       skip = 0                !starting sobol point
-      ALLOCATE (r(m,n))
+      end_val = 10.0          !Integral bounds
+      start_val = 0.0
+      int_range = end_val - start_val
+      ALLOCATE (r(m,n))       !allocate space for arrays
+      ALLOCATE (q(m,n))
 
       CALL i8_sobol_generate(m, n, skip, r)
-      PRINT *, r              ! print out point to see if it worked
+      q = r*int_range    ! we now have random points sampling whole integral bound
+
+     
+      f = 0.0D0
+      integral = 0.0
+      DO i=1,n
+          f = f+integrand(q(m,i))
+      END DO
+      f = f/n
+
+      integral = (end_val-start_val)*f
+
+      PRINT *, integral
+        
+
+
+CONTAINS
+
+      FUNCTION integrand(r) RESULT (values)
+        IMPLICIT NONE
+        DOUBLE PRECISION :: r
+        REAL :: values
+        values = (r**2)*EXP(-r)
+      END FUNCTION integrand
 
 END PROGRAM unif_sobol_points
