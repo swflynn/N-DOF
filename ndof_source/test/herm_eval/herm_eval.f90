bec1c3e0a8fdb714724d70d83c21b430858c4e39
swflynn <swflynn@uci.edu>
2017-03-09 22:09:04 -0800

    Evaluate (0:1) array of points for polynomial 0:9

--- ndof_source/test/herm_eval/herm_eval.f90
+++ ndof_source/test/herm_eval/herm_eval.f90
@@ -1,19 +1,36 @@
 PROGRAM herm_poly_eval
       IMPLICIT NONE
 
-      REAL, PARAMETER :: chi  = 5.0
-      INTEGER :: i
-      DOUBLE PRECISION, DIMENSION(0:9) :: herm
+      REAL, DIMENSION(0:1) :: chi 
+      INTEGER :: i, j
+      DOUBLE PRECISION, DIMENSION(0:9, 0:9) :: herm
 
-      herm(0) = 1
-      herm(1) = 2.0*chi
+      ! make an array with values (this would be our sobol numbers)
+      chi(0) = 3.0
+      chi(1) = 5.0
+
+      ! initialize the first 2 polynomials for recursion 
+      !I should go back and do this for a general case
+      !For now the specific definitions work
+      herm(0,0) = 1.0
+      herm(1,0) = 1.0
+
+      herm(0,1) = 2.0*chi(0)
+      herm(1,1) = 2.0*chi(1)
       
-       DO i = 2, 9
+     
+      DO i = 0, 1
+      PRINT *, herm(i,0)
+      PRINT *, herm(i,1)
 
-        herm(i) = 2.0*chi*herm(i-1) - 2.0*(i-1)*herm(i-2)
+         DO j = 2, 9
+
+             herm(i,j) = 2.0*chi(i)*herm(i, j-1) - 2.0*(j-1)*herm(i, j-2)
+             PRINT *, herm(i,j)
+
+         END DO
 
       END DO
-
-      PRINT *, herm
+    
 
 END PROGRAM herm_poly_eval
