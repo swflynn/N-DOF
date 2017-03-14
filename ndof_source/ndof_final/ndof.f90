869fbbfdf4ad9eb1b11e98737a54b0c621863836
swflynn <swflynn@uci.edu>
2017-03-13 18:00:35 -0700

    Unif_Sob_Seq of N-points, H_0-H_9 eval.

--- ndof_source/ndof_final/ndof.f90
+++ ndof_source/ndof_final/ndof.f90
@@ -1,50 +1,45 @@
-! This is the program main for ndof. 
-!Please see the test directory for documentation. 
+! Please see the test directory for documentation. 
+! Generate normal sobol sequence and evaluate hermite polynomials at each point
+! Code is written for 1 dimension only, would need to modify for more. 
 
 PROGRAM main
   USE sobol
   IMPLICIT NONE
 
-  INTEGER (kind=8) :: m, n, skip, i, j
+  INTEGER (kind=8) :: m, Nsobol, skip, i, j
   Double Precision, allocatable :: r(:,:)
-  REAL :: end_val, start_val, int_range, integral
-  DOUBLE PRECISION, DIMENSION(1:10, 1:10) :: herm
-! Hard code number of sobol points to evaluate here. 
-! Could generalize to n points, however not good for memory, as n gets massive.
+  DOUBLE PRECISION, DIMENSION(1:10) :: herm
 
-!===================taken from unif_sobol===================================!
+   m = 1                   
+   Nsobol = 100           
+   skip = 2              
 
-   m = 1                   !spatial dimension only works for 1 currently
-   n = 2               !number of points to generate
-   skip = 10                !starting sobol point
+   OPEN(unit=10, file='data.dat')
 
-   ALLOCATE (r(m,n))       !allocate space for sobol points array
+   ALLOCATE (r(m,Nsobol)) 
 
-   CALL i8_sobol_generate(m, n, skip, r) !populates r(m,n) 
+   CALL i8_sobol_generate(m, Nsobol, skip, r) 
 
-   PRINT *, 'here are your sobol points'
-   PRINT *, r
-   Print *, 'Next are the hermite evaluations'
-!===================taken from unif_sobol===================================!
+   Write(10,*), 'here are your sobol points'
+   WRITE(10,*), r
+   WRITE(10,*), 'Next are the hermite evaluations'
 
-!===================taken from herm_eval===================================!
-  DO i = 1, n              
-    herm(i,1) = 1.0             !initialize first 2 polynomials for recursion
-    herm(i,2) = 2.0*r(m,i)        !This is correct
+   DO i = 1, Nsobol              
+    herm(1) = 1.0             
+    herm(2) = 2.0*r(m,i)       
 
-    PRINT *, 'next point'
-
-    PRINT *, herm(i,1)
-    PRINT *, herm(i,2)
+    WRITE(10,*), 'next point', r(m,i)
+    WRITE(10,*), herm(1)
+    WRITE(10,*), herm(2)
 
     DO j = 3,10       
 
-      herm(i,j) = (2.0*r(m,i)*herm(i, j-1)) - (2.0*(j-2)*herm(i, j-2))
-      PRINT *, herm(i,j)
+      herm(j) = (2.0*r(m,i)*herm(j-1)) - (2.0*(j-2)*herm(j-2))
+      WRITE(10,*), herm(j)
 
     END DO
 
-  END DO
-!===================taken from herm_eval===================================!
+   END DO
+   CLOSE(10)
 
 END PROGRAM main
