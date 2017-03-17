da9a445aec70bd3767ed7046c80ee7fac8d184f7
swflynn <swflynn@uci.edu>
2017-03-16 18:12:12 -0700

    Calculate matrix elements, need to normalize

--- ndof_source/development/norm_sob_int/simple2/simple2.f90
+++ ndof_source/development/norm_sob_int/simple2/simple2.f90
@@ -3,11 +3,12 @@ PROGRAM int_test
   IMPLICIT NONE
 
   INTEGER :: d, Nsobol, n, i, j
-  INTEGER :: k, m
+  INTEGER :: k, m, o
+  INTEGER, PARAMETER :: deg = 3       ! set which polynomial we want to calculate to
   INTEGER*8 :: skip
   DOUBLE PRECISION, ALLOCATABLE:: norm(:,:)
   DOUBLE PRECISION, DIMENSION(1:10) :: herm
-  DOUBLE PRECISION, DIMENSION(1:10, 1:10) :: A_km
+  DOUBLE PRECISION, DIMENSION(1:10, 1:10) :: A
 
   d = 1                           
   Nsobol = 100000
@@ -16,37 +17,44 @@ PROGRAM int_test
   ALLOCATE (norm(d,Nsobol))
 
 OPEN(UNIT=10, FILE='data.dat')
+A=0d0
 !=========================Get each sobol pointpoint=========================!
   DO n = 1, Nsobol                
     CALL sobol_stdnormal(d,skip,norm(:,n))
   END DO
+norm=norm/sqrt(2.)
 !=========================Get each sobol pointpoint=========================!
 
 !=========================evaluate each sobol point=========================!
   DO i = 1, Nsobol              
         herm(1) = 1.0             
-        herm(2) = 2.0*norm(d,i)       
+        herm(2) = 2.0*norm(1,i)       
 
         
       !======================evaluate each herm for a single sobol point=========================!
-        DO j = 3,10       
+        DO j = 3,deg      
              herm(j) = (2.0*norm(d,i)*herm(j-1)) - (2.0*(j-2)*herm(j-2))
         END DO
       !=====================evaluate each herm for a single sobol point=========================!
       
       !=====================evaluate each matrix element for a single point=========================!
-        DO k = 1, 10
-             DO m = 1, 10
-                 A_km(k,m) = A_km(k,m) + herm(k)*herm(m)
+        DO k = 1, deg
+             DO m = 1, deg
+                 A(k,m) = A(k,m) + herm(k)*herm(m)
              END DO
         END DO
       !=====================evaluate each matrix element for a single point=========================!
 
   END DO
 !=========================evaluate each sobol point=========================!
-  A_km = A_km / Nsobol
+  A = A / Nsobol
 
-  Write(10,*) A_km
+  
+!=========================write out matrix elements=========================!
+  do o=1,deg
+     Write(10,*) A(1:deg,o)
+  enddo
+!=========================write out matrix elements=========================!
   
   CLOSE(10)
 
