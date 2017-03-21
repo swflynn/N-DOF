5e65950a1525e20183a380ee156f73776ce6db0c
swflynn <swflynn@uci.edu>
2017-03-20 17:13:22 -0700

    Matrix Elements for the 1D HO with Normal Sobol

--- ndof_source/test/herm_matrix/herm_mat.f90
+++ ndof_source/test/herm_matrix/herm_mat.f90
@@ -2,28 +2,39 @@ PROGRAM mat_eval
   USE sobol
   IMPLICIT NONE
 
-  INTEGER :: d, Nsobol, n, i, j
-  INTEGER :: k, m, o
-  INTEGER, PARAMETER :: deg = 3       ! set which polynomial we want to calculate to (3-10)
+  INTEGER :: d, Nsobol
+  INTEGER :: i, j, k, m, n, o, p      ! loop indexing
+  INTEGER, PARAMETER :: deg = 6       !polynomial we want to calculate to (3-10)
   INTEGER*8 :: skip
   DOUBLE PRECISION, ALLOCATABLE:: norm(:,:) !matrix of normal sobol points
-  DOUBLE PRECISION, DIMENSION(1:10) :: herm !matrix evalue harmite polynomials
+  DOUBLE PRECISION, DIMENSION(1:10) :: herm, coef !evaluate harmite polynomials, wavefn coef.
   DOUBLE PRECISION, DIMENSION(1:10, 1:10) :: A  !matrix elements <H_k|H_m>
 
   d = 1                           
   Nsobol = 100000
-  skip = 10                        
+  skip = 1000
 
   ALLOCATE (norm(d,Nsobol))
 
-OPEN(UNIT=10, FILE='data.dat')
-A=0d0
+  OPEN(UNIT=10, FILE='data.dat')
+  A=0d0                             !initialize matrix elements
 !=========================Get each sobol pointpoint=========================!
   DO n = 1, Nsobol                
     CALL sobol_stdnormal(d,skip,norm(:,n))
   END DO
-norm=norm/sqrt(2.)
+  norm=norm/sqrt(2.)
 !=========================Get each sobol pointpoint=========================!
+
+
+!=========================Get each wavefn coef.=========================!
+  coef(1) = 1
+  coef(2) = 1.0 / (SQRT(2.0))
+  DO p = 3,deg
+     coef(p) = coef(p-1)*(1 /SQRT(2.0*REAL(p-1)))
+  END DO
+ 
+!=========================Get each wavefn coef.=========================!
+
 
 !=========================evaluate each sobol point=========================!
   DO i = 1, Nsobol              
@@ -37,10 +48,10 @@ norm=norm/sqrt(2.)
         END DO
       !=====================evaluate each polynomial for a single sobol point=========================!
       
-      !=====================evaluate each matrix element for a single point=========================!
+      !=====================evaluate each wavefn matrix element for a single point=========================!
         DO k = 1, deg
              DO m = 1, deg
-                 A(k,m) = A(k,m) + herm(k)*herm(m)
+                 A(k,m) = A(k,m) + coef(k)*herm(k)*coef(m)*herm(m)
              END DO
         END DO
       !=====================evaluate each matrix element for a single point=========================!
