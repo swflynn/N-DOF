78b1d33afb8e5b331fa6c69d758035ebf005375d
swflynn <swflynn@uci.edu>
2017-03-20 17:14:53 -0700

    Matrix Elements for Wavefunction

--- ndof_source/development/norm_sob_int/simple2/simple2.f90
+++ ndof_source/development/norm_sob_int/simple2/simple2.f90
@@ -2,17 +2,17 @@ PROGRAM int_test
   USE sobol
   IMPLICIT NONE
 
-  INTEGER :: d, Nsobol, n, i, j
-  INTEGER :: k, m, o
-  INTEGER, PARAMETER :: deg = 3       ! set which polynomial we want to calculate to
+  INTEGER :: d, Nsobol
+  INTEGER :: n, i, j, k, m, o, p
+  INTEGER, PARAMETER :: deg = 6       !polynomial we want to calculate up to
   INTEGER*8 :: skip
   DOUBLE PRECISION, ALLOCATABLE:: norm(:,:)
-  DOUBLE PRECISION, DIMENSION(1:10) :: herm
+  DOUBLE PRECISION, DIMENSION(1:10) :: herm, coef
   DOUBLE PRECISION, DIMENSION(1:10, 1:10) :: A
 
   d = 1                           
   Nsobol = 100000
-  skip = 10                        
+  skip = 1000
 
   ALLOCATE (norm(d,Nsobol))
 
@@ -22,25 +22,35 @@ A=0d0
   DO n = 1, Nsobol                
     CALL sobol_stdnormal(d,skip,norm(:,n))
   END DO
-norm=norm/sqrt(2.)
+  norm=norm/SQRT(2.)      
 !=========================Get each sobol pointpoint=========================!
+
+!=========================Get each wavefn coef.=========================!
+  coef(1) = 1
+  coef(2) = 1.0 / (SQRT(2.0))
+  DO p = 3,deg
+    coef(p) = coef(p-1)*(1 /SQRT(2.0*REAL(p-1)))
+  END DO
+ 
+
+!=========================Get each wavefn coef.=========================!
+
 
 !=========================evaluate each sobol point=========================!
   DO i = 1, Nsobol              
         herm(1) = 1.0             
         herm(2) = 2.0*norm(1,i)       
-
         
       !======================evaluate each herm for a single sobol point=========================!
         DO j = 3,deg      
-             herm(j) = (2.0*norm(d,i)*herm(j-1)) - (2.0*(j-2)*herm(j-2))
+             herm(j) =(2.0*norm(d,i)*herm(j-1)) - (2.0*(j-2)*herm(j-2))
         END DO
       !=====================evaluate each herm for a single sobol point=========================!
       
       !=====================evaluate each matrix element for a single point=========================!
         DO k = 1, deg
              DO m = 1, deg
-                 A(k,m) = A(k,m) + herm(k)*herm(m)
+                 A(k,m) = A(k,m) + coef(k)*herm(k)*coef(m)*herm(m)
              END DO
         END DO
       !=====================evaluate each matrix element for a single point=========================!
@@ -48,7 +58,6 @@ norm=norm/sqrt(2.)
   END DO
 !=========================evaluate each sobol point=========================!
   A = A / Nsobol
-
   
 !=========================write out matrix elements=========================!
   do o=1,deg
