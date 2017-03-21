677bce70d03364d4ba1c10f11ec46158966f3d10
swflynn <swflynn@uci.edu>
2017-03-20 21:19:26 -0700

    HO Normal Sobol Matrix + Convergence(nsobol)

--- ndof_source/test/herm_matrix/herm_mat.f90
+++ ndof_source/test/herm_matrix/herm_mat.f90
@@ -4,19 +4,18 @@ PROGRAM mat_eval
 
   INTEGER :: d, Nsobol
   INTEGER :: i, j, k, m, n, o, p      ! loop indexing
-  INTEGER, PARAMETER :: deg = 6       !polynomial we want to calculate to (3-10)
+  INTEGER, PARAMETER :: deg = 10       !polynomial we want to calculate to (3-10)
   INTEGER*8 :: skip
   DOUBLE PRECISION, ALLOCATABLE:: norm(:,:) !matrix of normal sobol points
   DOUBLE PRECISION, DIMENSION(1:10) :: herm, coef !evaluate harmite polynomials, wavefn coef.
   DOUBLE PRECISION, DIMENSION(1:10, 1:10) :: A  !matrix elements <H_k|H_m>
 
   d = 1                           
-  Nsobol = 100000
+  Nsobol = 1000000
   skip = 1000
 
   ALLOCATE (norm(d,Nsobol))
 
-  OPEN(UNIT=10, FILE='data.dat')
   A=0d0                             !initialize matrix elements
 !=========================Get each sobol pointpoint=========================!
   DO n = 1, Nsobol                
@@ -35,6 +34,7 @@ PROGRAM mat_eval
  
 !=========================Get each wavefn coef.=========================!
 
+OPEN(UNIT=9, FILE='converge.dat')     !convergence data file 
 
 !=========================evaluate each sobol point=========================!
   DO i = 1, Nsobol              
@@ -42,30 +42,45 @@ PROGRAM mat_eval
         herm(2) = 2.0*norm(1,i)       
 
         
-      !==================evaluate each polynomial for a single sobol point=======================!
+!================evaluate each polynomial for a single sobol point===============!
         DO j = 3,deg      
              herm(j) = (2.0*norm(d,i)*herm(j-1)) - (2.0*(j-2)*herm(j-2))
         END DO
-      !=====================evaluate each polynomial for a single sobol point=========================!
+ !================evaluate each polynomial for a single sobol point==================!
       
-      !=====================evaluate each wavefn matrix element for a single point=========================!
+ !=============evaluate each wavefn matrix element for a single point==================!
         DO k = 1, deg
              DO m = 1, deg
                  A(k,m) = A(k,m) + coef(k)*herm(k)*coef(m)*herm(m)
              END DO
         END DO
-      !=====================evaluate each matrix element for a single point=========================!
+!=================evaluate each matrix element for a single point==================!
+
+
+  !=====================Convergence test at every i points=========================!
+  IF (mod(i,1000)==0) THEN
+    WRITE(9,*) A(4,4) / REAL(i), A(6,6)/REAL(i), A(8,8)/REAL(i), A(10,10)/REAL(i), & 
+  &            A(1,6) / REAL(i), A(1,10)/REAL(i), A(6,1)/REAL(i), A(10,1)/REAL(i), &
+  &            A(7,3) / REAL(i), A(6,8)/REAL(i), A(8,10)/REAL(i), A(9,4)/REAL(i)
+  END IF
+
+
+  !=====================Convergence test at every i points=========================!
 
   END DO
+
+  CLOSE(9)
 !=========================evaluate each sobol point=========================!
+ 
   A = A / Nsobol            !normalize integrals
 
   
-!=========================write out matrix elements in nice format=========================!
+!=========================write out matrix elements in nice format===================!
+  OPEN(UNIT=10, FILE='data.dat')
   do o=1,deg
      Write(10,*) A(1:deg,o)
   enddo
-!=========================write out matrix elements in nice format=========================!
+!=========================write out matrix elements in nice format===================!
   
   CLOSE(10)
 
