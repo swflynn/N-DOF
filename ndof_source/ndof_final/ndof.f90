6df90230fb778e0bb9740a4ba32da2800ab3a24d
swflynn <swflynn@uci.edu>
2017-03-20 17:27:35 -0700

    Evaluate 1D Normal Sobol HO Matrix Elements

--- ndof_source/ndof_final/ndof.f90
+++ ndof_source/ndof_final/ndof.f90
@@ -1,42 +1,59 @@
 ! Please see the test directory for documentation. 
-! Generate normal sobol sequence and evaluate hermite polynomials at each point
 ! Code is written for 1 dimension only, would need to modify for more. 
+! Generate norm sobol seq and evaluate each hermite polynomial
+! Then calculate the matrix elements for the HO wavefunction
 
 PROGRAM main
   USE sobol
   IMPLICIT NONE
 
-  INTEGER :: d, Nsobol, i, j, k
+  INTEGER :: d, Nsobol
+  INTEGER :: i, j, k, m, n, o, p      
+  INTEGER, PARAMETER :: deg = 10       !polynomial we want to calculate to (3-10)
   INTEGER*8 :: skip
-  DOUBLE PRECISION, ALLOCATABLE:: norm(:,:)
-  DOUBLE PRECISION, DIMENSION(1:10) :: herm
+  DOUBLE PRECISION, ALLOCATABLE:: norm(:,:) !matrix of normal sobol points
+  DOUBLE PRECISION, DIMENSION(1:10) :: herm, coef 
+  DOUBLE PRECISION, DIMENSION(1:10, 1:10) :: A  !matrix elements <H_k|e^-x^2/2|H_m>
 
   d = 1                           
-  Nsobol = 100                   
-  skip = 10                        
-
-  OPEN(UNIT=10, FILE='data.dat')
+  Nsobol = 10000000
+  skip = 1000
 
   ALLOCATE (norm(d,Nsobol))
 
-  DO i = 1, Nsobol                
-    CALL sobol_stdnormal(d,skip,norm(:,i))
-    WRITE(10,*) norm(:,i)
+  OPEN(UNIT=10, FILE='data.dat')
+  A=0d0                             
+
+  DO n = 1, Nsobol                
+    CALL sobol_stdnormal(d,skip,norm(:,n))
+  END DO
+  norm=norm/sqrt(2.)
+
+  coef(1) = 1
+  coef(2) = 1.0 / (SQRT(2.0))
+  DO p = 3,deg
+     coef(p) = coef(p-1)*(1 /SQRT(2.0*REAL(p-1)))
   END DO
 
-  DO j = 1, Nsobol              
-    herm(1) = 1.0             !initialize first 2 polynomials for recursion
-    herm(2) = 2.0*norm(d,j)       
-
-    WRITE(10,*), 'next point', norm(d,j)
-    WRITE(10,*), herm(1)
-    WRITE(10,*), herm(2)
-
-    DO k = 3,10       
-      herm(k) = (2.0*norm(d,j)*herm(k-1)) - (2.0*(k-2)*herm(k-2))
-      WRITE(10,*), herm(k)
-    END DO
+  DO i = 1, Nsobol              
+        herm(1) = 1.0             
+        herm(2) = 2.0*norm(1,i)       
+        DO j = 3,deg      
+             herm(j) = (2.0*norm(d,i)*herm(j-1)) - (2.0*(j-2)*herm(j-2))
+        END DO
+      
+        DO k = 1, deg
+             DO m = 1, deg
+                 A(k,m) = A(k,m) + coef(k)*herm(k)*coef(m)*herm(m)
+             END DO
+        END DO
   END DO
+  A = A / Nsobol            
+  
+  DO o=1,deg
+     WRITE(10,*) A(1:deg,o)
+     END DO
+  
   CLOSE(10)
 
 END PROGRAM main
