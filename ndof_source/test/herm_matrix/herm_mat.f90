349f03bffd9f3a273ddf8efbb08883779d36ed62
swflynn <swflynn@uci.edu>
2017-03-22 20:50:17 -0700

    1D Norm Sobol Analysis with Convergence

--- ndof_source/test/herm_matrix/herm_mat.f90
+++ ndof_source/test/herm_matrix/herm_mat.f90
@@ -1,87 +1,69 @@
 PROGRAM mat_eval
+  
   USE sobol
   IMPLICIT NONE
-
-  INTEGER :: d, Nsobol
-  INTEGER :: i, j, k, m, n, o, p      ! loop indexing
-  INTEGER, PARAMETER :: deg = 10       !polynomial we want to calculate to (3-10)
-  INTEGER*8 :: skip
-  DOUBLE PRECISION, ALLOCATABLE:: norm(:,:) !matrix of normal sobol points
-  DOUBLE PRECISION, DIMENSION(1:10) :: herm, coef !evaluate harmite polynomials, wavefn coef.
-  DOUBLE PRECISION, DIMENSION(1:10, 1:10) :: A  !matrix elements <H_k|H_m>
+  INTEGER :: d, Nsobol                  ! dimension and number of points
+  INTEGER :: n, i, j, k, m, o, p         !iterative variables
+  INTEGER, PARAMETER :: deg = 10       !polynomial we want to calculate up to
+  INTEGER*8 :: skip                    !set similar to Nsobol
+  DOUBLE PRECISION, ALLOCATABLE:: norm(:,:)   !vector of normal sobol points
+  DOUBLE PRECISION, DIMENSION(1:10) :: herm, coef    !hermite polynomial and coef vectors
+  DOUBLE PRECISION, DIMENSION(1:10, 1:10) :: A     ! matrix elements
 
   d = 1                           
-  Nsobol = 100000000
-  skip = 1000
-
+  Nsobol = 100
+  skip = 100
   ALLOCATE (norm(d,Nsobol))
-
-  A=0d0                             !initialize matrix elements
+  A=0d0
 !=========================Get each sobol pointpoint=========================!
   DO n = 1, Nsobol                
     CALL sobol_stdnormal(d,skip,norm(:,n))
   END DO
-  norm=norm/sqrt(2.)
+  norm=norm/SQRT(2.)      
 !=========================Get each sobol pointpoint=========================!
-
 
 !=========================Get each wavefn coef.=========================!
   coef(1) = 1
   coef(2) = 1.0 / (SQRT(2.0))
   DO p = 3,deg
-     coef(p) = coef(p-1)*(1 /SQRT(2.0*REAL(p-1)))
+    coef(p) = coef(p-1)*(1 /SQRT(2.0*REAL(p-1)))
   END DO
- 
-!=========================Get each wavefn coef.=========================!
-
-OPEN(UNIT=9, FILE='converge.dat')     !convergence data file 
-
+  !=========================Get each wavefn coef.=========================!
+  OPEN(UNIT=9, FILE='converge.dat')
 !=========================evaluate each sobol point=========================!
-  DO i = 1, Nsobol              
+DO i = 1, Nsobol              
         herm(1) = 1.0             
         herm(2) = 2.0*norm(1,i)       
-
-        
-!================evaluate each polynomial for a single sobol point===============!
+!=============evaluate each herm for a single sobol point===================!
         DO j = 3,deg      
-             herm(j) = (2.0*norm(d,i)*herm(j-1)) - (2.0*(j-2)*herm(j-2))
+             herm(j) =(2.0*norm(d,i)*herm(j-1)) - (2.0*(j-2)*herm(j-2))
         END DO
- !================evaluate each polynomial for a single sobol point==================!
-      
- !=============evaluate each wavefn matrix element for a single point==================!
+!=========evaluate each herm for a single sobol point======================!
+
+!==============evaluate each matrix element for a single point===================!
         DO k = 1, deg
              DO m = 1, deg
                  A(k,m) = A(k,m) + coef(k)*herm(k)*coef(m)*herm(m)
              END DO
         END DO
-!=================evaluate each matrix element for a single point==================!
+ !=============evaluate each matrix element for a single point======================!
 
-
-  !=====================Convergence test at every i points=========================!
-  IF (mod(i,1000)==0) THEN
-    WRITE(9,*) i, A(4,4) / REAL(i), A(6,6)/REAL(i), A(8,8)/REAL(i), A(10,10)/REAL(i), & 
-  &            A(1,6) / REAL(i), A(1,10)/REAL(i), A(6,1)/REAL(i), A(10,1)/REAL(i), &
-  &            A(7,3) / REAL(i), A(6,8)/REAL(i), A(8,10)/REAL(i), A(9,4)/REAL(i)
+! add in function to print results as a function of N
+  IF (mod(i,10)==0) THEN
+      WRITE(9,*) (A) / REAL(i)
   END IF
-
-
-  !=====================Convergence test at every i points=========================!
 
   END DO
 
   CLOSE(9)
 !=========================evaluate each sobol point=========================!
- 
-  A = A / Nsobol            !normalize integrals
-
-  
-!=========================write out matrix elements in nice format===================!
-  OPEN(UNIT=10, FILE='data.dat')
+  A = A / Nsobol
+!=========================write out matrix elements=========================!
+  OPEN(UNIT=10, FILE='final_matrix.dat')
   do o=1,deg
      Write(10,*) A(1:deg,o)
   enddo
-!=========================write out matrix elements in nice format===================!
-  
   CLOSE(10)
+!=========================write out matrix elements=========================!
 
 END PROGRAM mat_eval
