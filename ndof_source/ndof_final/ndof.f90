129b27833193c4b72f8092caca224b61a9287b1f
swflynn <swflynn@uci.edu>
2017-03-27 20:23:49 -0700

    Data Analysis; Nsobol Convergence

--- ndof_source/ndof_final/ndof.f90
+++ ndof_source/ndof_final/ndof.f90
@@ -16,24 +16,21 @@ PROGRAM main
   DOUBLE PRECISION, DIMENSION(1:10, 1:10) :: A
 
   d = 1                           
-  Nsobol = 100
-  skip = 100
+  Nsobol = 1000000000
+  skip = 1000000000
   ALLOCATE (norm(d,Nsobol))
   A=0d0
-
 !=============Get each sobol point (normal distribution)====================!
   DO n = 1, Nsobol                
     CALL sobol_stdnormal(d,skip,norm(:,n))
   END DO
   norm=norm/SQRT(2.)      
-
 !===========Recursively Calculate HO Coefficients===========================!
   coef(1) = 1
   coef(2) = 1.0 / (SQRT(2.0))
   DO p = 3,deg
     coef(p) = coef(p-1)*(1 /SQRT(2.0*REAL(p-1)))
   END DO
-
 !=========================evaluate each sobol point=========================!
   OPEN(UNIT=9, FILE='converge.dat')
   DO i = 1, Nsobol              
@@ -42,27 +39,24 @@ PROGRAM main
         DO j = 3,deg      
              herm(j) =(2.0*norm(d,i)*herm(j-1)) - (2.0*(j-2)*herm(j-2))
         END DO
-
 !=========evaluate each matrix element using the HO wavefunction==============!
         DO k = 1, deg
              DO m = 1, deg
                  A(k,m) = A(k,m) + coef(k)*herm(k)*coef(m)*herm(m)
              END DO
         END DO
-
 !=========Convergence Analysis at n points, remove if not needed==========!
-  IF (mod(i,10)==0) THEN
+  IF (mod(i,10000)==0) THEN
       WRITE(9,*) A / REAL(i)
   END IF
 
   END DO
   CLOSE(9)
-  
   A = A / Nsobol
+!=========Write out the final Matrix==========!
   OPEN(UNIT=10, FILE='mat_elem.dat')
-  do o=1,deg
+  DO o=1,deg
      Write(10,*) A(1:deg,o)
-  enddo
+  END DO
   CLOSE(10)
-
 END PROGRAM main
