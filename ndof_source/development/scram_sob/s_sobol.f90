01be9165657c4aa2f46592acd9aa3768f67258f9
swflynn <swflynn@uci.edu>
2017-03-30 22:00:01 -0700

    Scrambled Sobol Analysis Development

--- ndof_source/development/scram_sob/s_sobol.f90
+++ ndof_source/development/scram_sob/s_sobol.f90
@@ -1,46 +1,47 @@
 PROGRAM s_mat_eval
+
  IMPLICIT NONE
-  
  INTEGER :: d, Nsobol
- INTEGER, PARAMETER :: deg = 10
- INTEGER :: n, p, i, j, k, m, q
- DOUBLE PRECISION, ALLOCATABLE, DIMENSION(:,:) :: scrambled_u
- DOUBLE PRECISION, ALLOCATABLE, DIMENSION(:,:) :: scrambled_z
- DOUBLE PRECISION, DIMENSION(1:10) :: herm, coef 
- DOUBLE PRECISION, DIMENSION(1:10, 1:10) :: A
+ INTEGER, PARAMETER :: deg = 3
+ INTEGER :: i, j, k, m
+ DOUBLE PRECISION, ALLOCATABLE :: scrambled_u(:,:), scrambled_z(:,:), herm(:), coef(:), A(:,:)
 
  d=1
- Nsobol=10000
- 
- ALLOCATE(scrambled_u(d, Nsobol))
- ALLOCATE(scrambled_z(d, Nsobol))
- 
- OPEN(UNIT=6, FILE='s_sobol_unif.dat', STATUS='OLD', ACTION='READ')
- READ(6,*) scrambled_u
- CLOSE(UNIT=6)
+ Nsobol=100000
+ ALLOCATE(scrambled_u(d, Nsobol), scrambled_z(d, Nsobol), herm(deg), coef(deg), A(deg,deg))
+
+ herm = 0d0
+ coef = 0d0
+ A = 0d0
+ scrambled_u = 0d0
+ scrambled_z = 0d0
+
+ OPEN(UNIT=70, FILE='s_sobol_unif.dat', STATUS='OLD', ACTION='READ')
+ READ(70,*) scrambled_u
+ CLOSE(UNIT=70)
 
  !=========================Get each sobol point normal_dist=========================!
- DO n = 1, Nsobol
-    CALL scrambled_sobol_stdnormal(d, scrambled_u(:,n), scrambled_z(:,n))
+ DO i = 1, Nsobol
+    CALL scrambled_sobol_stdnormal(d, scrambled_u(:,i), scrambled_z(:,i))
  END DO
  scrambled_z = scrambled_z/SQRT(2.)
  !=========================Get each sobol point normal_dist=========================!
 
 !=========================Get each wavefn coef.=========================!
   coef(1) = 1.0
-  coef(2) = 1.0 / (SQRT(2.0))
-  DO p = 3,deg
-    coef(p) = coef(p-1)*(1 /SQRT(2.0*REAL(p-1)))
+  coef(2) = 1.0 / SQRT(2.)
+  DO i = 3,deg
+    coef(i) = coef(i-1)*(1 / SQRT(2.*(i-1)))
   END DO
 !=========================Get each wavefn coef.=========================!
-  OPEN(UNIT=9, FILE='converge.dat') 
+  OPEN(UNIT=75, FILE='converge.dat') 
 !=========================evaluate each sobol point=========================!
-DO i = 1, Nsobol              
+  DO i = 1, Nsobol              
         herm(1) = 1.0             
         herm(2) = 2.0*scrambled_z(1,i)       
 !=============evaluate each herm for a single sobol point===================!
         DO j = 3,deg      
-             herm(j) =(2.0*scrambled_z(d,i)*herm(j-1)) - (2.0*(j-2)*herm(j-2))
+             herm(j) =(2.*scrambled_z(d,i)*herm(j-1)) - (2.*(j-2)*herm(j-2))
         END DO
 !=========evaluate each herm for a single sobol point======================!
 
@@ -53,27 +54,24 @@ DO i = 1, Nsobol
  !=============evaluate each matrix element for a single point======================!
 
 ! add in function to print results as a function of N
-  IF (mod(i,100)==0) THEN
-      WRITE(9,*) (A) / REAL(i)
+  IF (mod(i,1000)==0) THEN
+      WRITE(75,*) (A) / i
   END IF
 
   END DO
 
-  CLOSE(UNIT=9)
 !=========================evaluate each sobol point=========================!
+  CLOSE(UNIT=75)
+
   A = A / Nsobol 
  
-!=========================write out matrix elements=========================!
-  OPEN(UNIT=10, FILE='final_matrix.dat')
-  do q=1,deg
-     Write(10,*) A(1:deg,q)
-  enddo
-  CLOSE(UNIT=10)
+!=========================write out final matrix elements=========================!
+  OPEN(UNIT=80, FILE='final_matrix.dat')
+  DO i=1,deg
+     Write(80,*) A(1:deg,i)
+  END DO
 !=========================write out matrix elements=========================! 
- 
- 
- DEALLOCATE(scrambled_u)
- DEALLOCATE(scrambled_z)
-
+  CLOSE(UNIT=80)
+  DEALLOCATE(scrambled_u, scrambled_z, herm, coef, A)
  
 END PROGRAM s_mat_eval
--- ndof_source/development/scram_sob/s_sobol.m
+++ ndof_source/development/scram_sob/s_sobol.m
@@ -1,8 +1,8 @@
 % Script to generate scrambled sobol sequence in matlab
 
 %define points, skip, dimensionality of the sequence
-Nsobol = 10000;
-Nsobol_skip = 10000;
+Nsobol = 100000;
+Nsobol_skip = 100000;
 d = 1;
 
 % generate the sequence and save it to a file
