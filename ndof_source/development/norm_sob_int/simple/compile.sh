b207e26ec18e3b637cf9a7eadb7e7eb123888fcc
swflynn <swflynn@uci.edu>
2017-03-31 14:06:15 -0700

    Clean up development directory

--- ndof_source/development/norm_sob_int/simple/compile.sh
+++
@@ -1,6 +0,0 @@
-rm *.mod *.o *.out
-gfortran -c sobol.f90
-gfortran -c sobol_stdnormal.f90
-gfortran -c simple.f90
-gfortran simple.f90 sobol.o sobol_stdnormal.o
-./a.out
--- ndof_source/development/norm_sob_int/simple2/compile.sh
+++
@@ -1,6 +0,0 @@
-rm *.mod *.o *.out *.dat
-gfortran -c sobol.f90
-gfortran -c sobol_stdnormal.f90
-gfortran -c simple2.f90
-gfortran simple2.f90 sobol.o sobol_stdnormal.o
-./a.out
