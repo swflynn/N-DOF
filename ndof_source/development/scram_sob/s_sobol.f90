edd5b2d39f80a47f838e36d5f72423bfee3389c8
swflynn <swflynn@uci.edu>
2017-03-31 10:40:31 -0700

    1D Scrambled Sobol Analysis

--- ndof_source/development/scram_sob/s_sobol.f90
+++ ndof_source/development/scram_sob/s_sobol.f90
@@ -2,30 +2,25 @@ PROGRAM s_mat_eval
 
  IMPLICIT NONE
  INTEGER :: d, Nsobol
- INTEGER, PARAMETER :: deg = 3
+ INTEGER, PARAMETER :: deg = 10
  INTEGER :: i, j, k, m
- DOUBLE PRECISION, ALLOCATABLE :: scrambled_u(:,:), scrambled_z(:,:), herm(:), coef(:), A(:,:)
+ REAL :: initial_time, final_time
+ DOUBLE PRECISION, ALLOCATABLE :: scrambled_u(:,:), scrambled_z(:), herm(:), coef(:), A(:,:)
+
+  CALL CPU_TIME(initial_time)
 
  d=1
  Nsobol=100000
- ALLOCATE(scrambled_u(d, Nsobol), scrambled_z(d, Nsobol), herm(deg), coef(deg), A(deg,deg))
+ ALLOCATE(scrambled_u(d, Nsobol), scrambled_z(d), herm(deg), coef(deg), A(deg,deg))
 
- herm = 0d0
- coef = 0d0
  A = 0d0
- scrambled_u = 0d0
- scrambled_z = 0d0
-
+ 
+ !=========================Read in Scrambled Sequence=========================!
  OPEN(UNIT=70, FILE='s_sobol_unif.dat', STATUS='OLD', ACTION='READ')
  READ(70,*) scrambled_u
  CLOSE(UNIT=70)
 
- !=========================Get each sobol point normal_dist=========================!
- DO i = 1, Nsobol
-    CALL scrambled_sobol_stdnormal(d, scrambled_u(:,i), scrambled_z(:,i))
- END DO
- scrambled_z = scrambled_z/SQRT(2.)
- !=========================Get each sobol point normal_dist=========================!
+ !=========================Convert to normal dist=========================!
 
 !=========================Get each wavefn coef.=========================!
   coef(1) = 1.0
@@ -33,34 +28,33 @@ PROGRAM s_mat_eval
   DO i = 3,deg
     coef(i) = coef(i-1)*(1 / SQRT(2.*(i-1)))
   END DO
-!=========================Get each wavefn coef.=========================!
+
   OPEN(UNIT=75, FILE='converge.dat') 
 !=========================evaluate each sobol point=========================!
   DO i = 1, Nsobol              
+    CALL scrambled_sobol_stdnormal(d, scrambled_u(:,i), scrambled_z(d))
+        scrambled_z = scrambled_z/SQRT(2.)
         herm(1) = 1.0             
-        herm(2) = 2.0*scrambled_z(1,i)       
+        herm(2) = 2.0*scrambled_z(d)       
 !=============evaluate each herm for a single sobol point===================!
         DO j = 3,deg      
-             herm(j) =(2.*scrambled_z(d,i)*herm(j-1)) - (2.*(j-2)*herm(j-2))
+             herm(j) =(2.*scrambled_z(d)*herm(j-1)) - (2.*(j-2)*herm(j-2))
         END DO
-!=========evaluate each herm for a single sobol point======================!
-
+        herm(:)=herm(:)*coef(:)
 !==============evaluate each matrix element for a single point===================!
         DO k = 1, deg
              DO m = 1, deg
-                 A(k,m) = A(k,m) + coef(k)*herm(k)*coef(m)*herm(m)
+                 A(k,m) = A(k,m) + herm(k)*herm(m)
              END DO
         END DO
- !=============evaluate each matrix element for a single point======================!
 
-! add in function to print results as a function of N
-  IF (mod(i,1000)==0) THEN
+!==============Matrix Convergence as a function of N===================!
+  IF (mod(i,100)==0) THEN
       WRITE(75,*) (A) / i
   END IF
 
   END DO
 
-!=========================evaluate each sobol point=========================!
   CLOSE(UNIT=75)
 
   A = A / Nsobol 
@@ -70,8 +64,12 @@ PROGRAM s_mat_eval
   DO i=1,deg
      Write(80,*) A(1:deg,i)
   END DO
-!=========================write out matrix elements=========================! 
+
   CLOSE(UNIT=80)
   DEALLOCATE(scrambled_u, scrambled_z, herm, coef, A)
+
+   CALL CPU_TIME(final_time)
+   WRITE(*,*) 'TOTAL TIME: ', final_time - initial_time
  
 END PROGRAM s_mat_eval
+
--- ndof_source/development/scram_sob/s_sobol.m
+++ ndof_source/development/scram_sob/s_sobol.m
@@ -2,11 +2,11 @@
 
 %define points, skip, dimensionality of the sequence
 Nsobol = 100000;
-Nsobol_skip = 100000;
+Nsobol_skip = 10000;
 d = 1;
 
 % generate the sequence and save it to a file
-points = sobolset(d, 'Skip', Nsobol_skip);
-points = scramble(points, 'MatousekAffineOwen');
-sequence = net(points, Nsobol); 
-dlmwrite('s_sobol_unif.dat', sequence , ' ');
+points = (sobolset(d, 'Skip', Nsobol_skip));
+points = (scramble(points, 'MatousekAffineOwen'));
+sequence = double(net(points, Nsobol));
+dlmwrite('s_sobol_unif.dat', sequence , 'delimiter', ' ', 'precision', 15);
