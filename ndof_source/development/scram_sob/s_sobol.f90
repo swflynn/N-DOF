96e5e356474e0e3c2f784f528e61dac7a1902fd3
swflynn <swflynn@uci.edu>
2017-04-13 11:38:42 -0700

    Generalize Dimension

--- ndof_source/development/scram_sob/s_sobol.f90
+++ ndof_source/development/scram_sob/s_sobol.f90
@@ -1,17 +1,17 @@
 PROGRAM s_mat_eval
 
  IMPLICIT NONE
- INTEGER :: d, Nsobol
- INTEGER, PARAMETER :: deg = 10
+ INTEGER :: Nsobol
+ INTEGER, PARAMETER :: deg = 3
+ INTEGER, PARAMETER :: s_dim = 1
  INTEGER :: i, j, k, m
  REAL :: initial_time, final_time
  DOUBLE PRECISION, ALLOCATABLE :: scrambled_u(:,:), scrambled_z(:), herm(:), coef(:), A(:,:)
 
   CALL CPU_TIME(initial_time)
 
- d=1
  Nsobol=100000
- ALLOCATE(scrambled_u(d, Nsobol), scrambled_z(d), herm(deg), coef(deg), A(deg,deg))
+ ALLOCATE(scrambled_u(s_dim, Nsobol), scrambled_z(s_dim), herm(deg), coef(deg), A(deg,deg))
 
  A = 0d0
  
@@ -32,13 +32,13 @@ PROGRAM s_mat_eval
   OPEN(UNIT=75, FILE='converge.dat') 
 !=========================evaluate each sobol point=========================!
   DO i = 1, Nsobol              
-    CALL scrambled_sobol_stdnormal(d, scrambled_u(:,i), scrambled_z(d))
+    CALL scrambled_sobol_stdnormal(s_dim, scrambled_u(:,i), scrambled_z(s_dim))
         scrambled_z = scrambled_z/SQRT(2.)
         herm(1) = 1.0             
-        herm(2) = 2.0*scrambled_z(d)       
+        herm(2) = 2.0*scrambled_z(s_dim)       
 !=============evaluate each herm for a single sobol point===================!
         DO j = 3,deg      
-             herm(j) =(2.*scrambled_z(d)*herm(j-1)) - (2.*(j-2)*herm(j-2))
+             herm(j) =(2.*scrambled_z(s_dim)*herm(j-1)) - (2.*(j-2)*herm(j-2))
         END DO
         herm(:)=herm(:)*coef(:)
 !==============evaluate each matrix element for a single point===================!
@@ -49,7 +49,7 @@ PROGRAM s_mat_eval
         END DO
 
 !==============Matrix Convergence as a function of N===================!
-  IF (mod(i,100)==0) THEN
+  IF (mod(i,1000000)==0) THEN
       WRITE(75,*) (A) / i
   END IF
 
