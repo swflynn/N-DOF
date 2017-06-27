68e3c64aedad3b9d745a49d687609ed1788929ec
swflynn <swflynn@uci.edu>
2017-06-27 16:48:47 -0700

    Hermite Polynomial Coeficients

--- ndof_source/scripts/Hermite_Coef/hermite_polynomial.f90
+++ ndof_source/scripts/Hermite_Coef/hermite_polynomial.f90
@@ -1,32 +1,24 @@
-! Generate Hermite Polynomial coeficients, only for H_0 - H_10
-!The code asks use for what degree polynomial they want to calculate
-! This can quickly generate coeficients for user to look at. 
-! Should not be used in real calculations, better to use recursions.
-! See unif_sob_herm for matrix calculations
-! Code is working as of 2-17-17. -Shane
+! Fortran-90 program to determine the first 10 Hermite Polynomials Coefficients
+
+!The code asks what degree polynomial you are interested in, and provides the coeficients for each polynomial up to the specified degree. 
 
 PROGRAM Hermite
   IMPLICIT NONE
 
   REAL*8 :: A(0:10)      !Final coeficient for each degree
   REAL*8 :: B(0:10,0:10) 
-  INTEGER :: degree,k    !k = dummy
+  INTEGER :: degree,k    
   CHARACTER :: x
 
   PRINT *, "What degree polynomial would you like to consider" 
   READ (*,*) degree           !which Hermite polynomial you want
-     WRITE(*,25) degree ! write what degree polynomial you are calculating
-
-     CALL Hermite_Coeff(degree,A,B) ! call fn to generate polynomial
-
+     WRITE(*,25) degree !write what degree polynomial you are calculating
+     CALL Hermite_Coeff(degree,A,B) 
      DO k = 0, degree
        WRITE(*,50) k, A(k)  ! write out all of our polynomial coefficients
      END DO 
 
-   stop
-
-!Pretty formatting for writing coefficients to screen called above
-
+!Formatting for writing coefficients to screen 
 25 format('Coefficients for Hermite Polynomial',i2/)
 50 format('A(',i2,') = ',f10.0)
 
@@ -35,7 +27,7 @@ END PROGRAM HERMITE
 SUBROUTINE Hermite_Coeff(degree,A,B)   !Calculate our coefficients
   IMPLICIT NONE
   INTEGER i,j,degree
-  REAL*8  A(0:10), B(0:10,0:10)  ! A = 1D array, B = 2D array 
+  REAL*8  A(0:10), B(0:10,0:10)  
   B(0,0)=1.D0 ; B(1,0)=0.D0 ; B(1,1)=2.D0 ! Recursion requires first terms
 
  IF (degree == 0) THEN  ! Zeroth polynomial H_0 = 1
@@ -51,9 +43,6 @@ SUBROUTINE Hermite_Coeff(degree,A,B)   !Calculate our coefficients
     DO i = 2, degree       ! loop from 2-degree
       B(i,0)=-2.D0*(i-1)*B(i-2,0) !Determines x**0 term (constant term)
       DO j = 1, i    !Determines the value for each power of x
-
-!Basic recursion relation
-
         B(i,j)=2.D0*B(i-1,j-1)-2.D0*(i-1)*B(i-2,j)
       END dO 
     END DO 
@@ -65,3 +54,5 @@ SUBROUTINE Hermite_Coeff(degree,A,B)   !Calculate our coefficients
   RETURN 
 
 END SUBROUTINE Hermite_Coeff
+
+! Code is working as of 2-17-17. -Shane
