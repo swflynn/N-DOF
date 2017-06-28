31a5fec90cfda7d193ea3bcebf880c50c487e51c
swflynn <swflynn@uci.edu>
2017-06-28 14:57:43 -0700

    1D Sobol Sequence. HO Wavefunction Convergence

--- ndof_source/scripts/1D_HO_matrix/compile.sh
+++ ndof_source/scripts/1D_HO_matrix/compile.sh
@@ -2,5 +2,5 @@ rm *.mod *.o *.out *.dat
 gfortran -c sobol.f90
 gfortran -c sobol_stdnormal.f90
 gfortran -c herm_mat.f90
-gfortran herm_mat.f90 sobol.o sobol_stdnormal.o
-./a.out
+gfortran -O herm_mat.f90 sobol.o sobol_stdnormal.o
+./a.out &
