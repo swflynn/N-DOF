75262b1f3cba641ab47f9d1f4772a8d6a17cda45
swflynn <swflynn@uci.edu>
2017-06-28 16:03:09 -0700

    1D Scrambled Sobol. HO Wavefunction ?Convergence

--- ndof_source/scripts/1D_s_HO_matrix/s_sobol.f90
+++ ndof_source/scripts/1D_s_HO_matrix/s_sobol.f90
@@ -1,32 +1,48 @@
-! Scrambled sobol sequence from matlab and calculates HO matrix using scrambled sobol sequence from matlab.
-! Currently works for 1D case only, and deg, Nsobol.
-!To use: 
-! Matlab to generate scrambled sobol sequence (.m file). 
-! Then set Nsobol appropriately here, and choose your deg (polynomial to calculate)
-! Set the matrix convergence to write as often as desired
-! Note; the scrambled case no longer needs sobol.f90 functions/subroutines, uses the scrambled function in sobol_stdnormal.f90
+!Code to calculate 1D-HO matrix elements using Scrambled Sobol Sequence 
+! Scrambled Sequence must first be generated using Matlab (s_sobol_unif.dat)
+!Matrix Element convergence analysis 
+
+!=========================================To Use=============================================!
+! First use the matlab script to generate your sobol points (s_sobol.m)
+!Set what degree (deg) polynomial to calculate up to
+!===============  NOTE: Indexing starts at 1 therefore deg = 10 := H_9 ======================!
+!Set the Nsobol number of points to use for the integration (the number you define above)
+!===============  NOTE: Matrix Elements for Spatial Dimension (d) = 1 only===================!
+! User should also set an appropriate number of convergence evaluations
+! Scrambled no longer needs sobol.f90 module, use the scrambled function in sobol_stdnormal.f90
+
+! Read in the scrambled sobol sequence data file
+! Recursion is used to calculate the HO Wavefunction Coefficients
+! Recursion is used to calculate the Hermite Poynomials 
+! The HO Wavefucntion is contructed as the product of the Coeficients and Polynomials
+!=========================================To Use=============================================!
 
 PROGRAM s_mat_eval
 
  IMPLICIT NONE
- INTEGER :: d, Nsobol
- INTEGER, PARAMETER :: deg = 10
+ INTEGER ::  Nsobol
+ INTEGER :: deg                               
+ INTEGER, PARAMETER :: d = 1                      ! Code for 1D case only
  INTEGER :: i, j, k, m
  REAL :: initial_time, final_time
  DOUBLE PRECISION, ALLOCATABLE :: scrambled_u(:,:), scrambled_z(:), herm(:), coef(:), A(:,:)
 
  CALL CPU_TIME(initial_time)
- d=1
- Nsobol=100000
+
+!=============================Variables to be set by User==================================!
+ Nsobol=100
+ deg = 10
+!=============================Variables to be set by User==================================!
+
  ALLOCATE(scrambled_u(d, Nsobol), scrambled_z(d), herm(deg), coef(deg), A(deg,deg))
  A = 0d0
  
- !=========================Read in Scrambled Sequence=========================!
+ !==============================Read in Scrambled Sequence=================================!
  OPEN(UNIT=70, FILE='s_sobol_unif.dat', STATUS='OLD', ACTION='READ')
  READ(70,*) scrambled_u
  CLOSE(UNIT=70)
 
-!=========================Get each wavefn coef.=========================!
+!====================================Get each wavefn coef.=================================!
   coef(1) = 1.0
   coef(2) = 1.0 / SQRT(2.)
   DO i = 3,deg
@@ -34,12 +50,13 @@ PROGRAM s_mat_eval
   END DO
 
   OPEN(UNIT=75, FILE='converge.dat') 
-!================evaluate each sobol point normal dist.======================!
+
+!========================evaluate each sobol point normal dist.============================!
   DO i = 1, Nsobol              
     CALL scrambled_sobol_stdnormal(d, scrambled_u(:,i), scrambled_z(d))
         scrambled_z = scrambled_z/SQRT(2.)
 
-!=============evaluate each herm for a single sobol point===================!
+!====================evaluate each herm for a single sobol point===========================!
         herm(1) = 1.0             
         herm(2) = 2.0*scrambled_z(d)       
         DO j = 3,deg      
@@ -47,14 +64,14 @@ PROGRAM s_mat_eval
         END DO
         herm(:)=herm(:)*coef(:)
 
-!========evaluate each matrix element for a single sobol point==============!
+!===============evaluate each matrix element for a single sobol point======================!
         DO k = 1, deg
              DO m = 1, deg
                  A(k,m) = A(k,m) + herm(k)*herm(m)
              END DO
         END DO
 
-!==============Matrix Convergence as a function of N===================!
+!=======================Matrix Convergence as a function of N==============================!
   IF (mod(i,100)==0) THEN
       WRITE(75,*) (A) / i
   END IF
@@ -65,7 +82,7 @@ PROGRAM s_mat_eval
 
   A = A / Nsobol 
  
-!=========================write out final matrix elements=========================!
+!==============================write out final matrix elements=============================!
   OPEN(UNIT=80, FILE='final_matrix.dat')
   DO i=1,deg
      Write(80,*) A(1:deg,i)
@@ -75,7 +92,14 @@ PROGRAM s_mat_eval
 
   DEALLOCATE(scrambled_u, scrambled_z, herm, coef, A)
   CALL CPU_TIME(final_time)
-  WRITE(*,*) 'TOTAL TIME (s): ', final_time - initial_time
 
+!=====================================Output File==========================================!
+OPEN(UNIT=83, FILE='output.dat')
+WRITE(83,*) 'Sobol Numers = ', Nsobol
+WRITE(83,*) 'Polynomial Degree= ', deg
+WRITE(83,*) 'This calculation ran for (s): ', final_time - initial_time
+CLOSE(UNIT=83)
+
+  WRITE(*,*) 'TOTAL TIME (s): ', final_time - initial_time
  
 END PROGRAM s_mat_eval
