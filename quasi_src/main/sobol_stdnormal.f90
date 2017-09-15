80eb89028e87b45f6652ef3dbd89cb8e130cba6a
swflynn <swflynn@uci.edu>
2017-09-15 13:23:50 -0700

    Update uniform to normal transformation

--- quasi_src/main/sobol_stdnormal.f90
+++ quasi_src/main/sobol_stdnormal.f90
@@ -1,6 +1,7 @@
-
-!> Computes the inverse cumulative density function (CDF), i.e., the quantile,
-! of the standard normal distribution given u uniform on the unit hypercube.
+!========================================================================================!
+!=====================Computes inverse cumulative density function=======================!
+!================of standard normal distribution ofuniform unit hypercube================!
+!========================================================================================!
 FUNCTION beasley_springer_moro(u) result(x)
     IMPLICIT NONE
 
@@ -34,7 +35,6 @@ FUNCTION beasley_springer_moro(u) result(x)
             0.0000002888167364, &
             0.0000003960315187 /)
 
-
     y = u - 0.5D0
 
     DO j = 1, SIZE(u)
@@ -55,29 +55,17 @@ FUNCTION beasley_springer_moro(u) result(x)
     END DO
 
 END FUNCTION beasley_springer_moro
-
-!==============================================================================!
-
-
-!> Returns a d-dimensional Sobol sequence of p points following a standard
-!  normal distribution
-!subroutine sobol_stdnormal(d, skip, x_stdnormal)
+!========================================================================================!
+!========= Returns a d-dimensional Sobol sequence following a std normal dist============!
+!=======================Code Has Been Modified for ssobol.f==============================!
+!========================================================================================!
 subroutine sobol_stdnormal(d, x_stdnormal)
-!    use sobol
     implicit none
-    !> dimension
     INTEGER(kind = 4), INTENT(IN) :: d
- 
-    !> number of initial points to be skipped
-!    INTEGER(kind = 8), INTENT(IN) :: skip   
-
-    !> return an array of doubles, standard normal
-!    DOUBLE PRECISION, DIMENSION(d), INTENT(OUT) :: x_stdnormal     
     DOUBLE PRECISION, DIMENSION(d), INTENT(INOUT) :: x_stdnormal    
   
     interface
         FUNCTION beasley_springer_moro(u)
-!            double precision :: u(:)            !VM changed
             double precision, INTENT(IN) :: u(:)
             double precision :: beasley_springer_moro(size(u))
         end function beasley_springer_moro
