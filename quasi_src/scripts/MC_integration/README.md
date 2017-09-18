9eef1ac1b2ddbdfd3ecece859a2126b0217e4db5
swflynn <swflynn@uci.edu>
2017-09-17 20:56:12 -0700

    Numerical Integration Script

---
+++ quasi_src/scripts/MC_integration/README.md
@@ -0,0 +1,50 @@
+# Program Main:
+Code curently takes in the number of spatial dimensions and maximum excitation to evaluate the number of permutations available to the system. 
+For each sobol point (standard Sobol Points only), it then evaluates all of the hermite polynomial products and evaluates the potential energy matrix. 
+Eigenvalues and matrix elements are provided as a function of iteration to track convergence. 
+There is no external field, therefore all integrals are of orthonormal functions.
+All diagonal elements should be 1, off-diagonal 0, and the eigenvalues should all be 1. 
+Use this test to determine the time and number of sobol points required for highly oscilatory matrix elements with higher excitation and spacial dimensions. 
+
+## Program Files
+This current code requires the following files for compilation and execution.
+
+### sobol.f90
+Old Fortran code for generating scrambled and non-scrambled sobol points (FSU John Burkardt). 
+The code is used to run the `i8_sobol` function called in the `sobol_stdnormal` subroutine.
+
+### Compile.sh
+A sample bash compile file for running the program. Simply run
+`$ bash compile.sh` at the terminal to execute.
+
+### uniform_sobol.f90
+The main program. 
+#### Important Variables:
+`d`: Integer, defines the spatial dimension, which dictates the length of the sobol point vector. 
+
+`Vmax`: Integer, sets the maximum excitation available to the system for evaluating permutations.
+Vmax must be a value between 1,9 or else `permutation` subroutine will not run. 
+
+`Jmax`: Integer containing the total number of permutations, given d and Vmax. 
+You program will contain Jmax eigenvalues and a square potential energy matrix of size Jmax.
+
+`v(d,Vmax)`: Integer, contains all of the permutation indices.
+
+`Nsobol`: Integer, number of sobol points to be generated for the entire simulation.
+This value must be less than 2**30 for the ssobol.f code to execute. 
+
+`skip`: REAL*8, starting location for the quasi-random sequence.
+Suggested to be equal to the value of Nsobol. 
+
+`scrambled_z(d)`: DP, vector containing a unique sobol point for each spatial dimension. 
+
+`herm(Vmax,d)`: DP evaluating each hermite polynomial recursively up to Vmax (index starts from 0). 
+
+`A(Vmax,Vmax,d)`: DP, contains all of the hermite polynomial permutation products. 
+This is done for evaluating our integrand as a product over the spatial dimensions. 
+
+`U(Jmax,Jmax)`: DP, Potential Energy Matrix Elements. 
+
+## User Inputs:
+The user should choose values for the following:
+d, Vmax, Nsobol, and skip, also set your convergence output interval. 
--- quasi_src/scripts/MC_integration/uniform_sobol.f90
+++ quasi_src/scripts/MC_integration/uniform_sobol.f90
@@ -1,13 +1,3 @@
-! Code to calculate integral of a function using uniform distribution of sobol points
-
-! Call the sobol.f90 module to generate a sequence of sobol points
-!Then do MC integration numerically over a function defined in the program. 
-
-!======================================TO USE================================================! 
-!Specify spatial dimension: m, Number of Sobol Points to generate: n, Skip when generating: seed (set the same as n)
-! Output prints integral of interest numerical solution
-!======================================TO USE================================================! 
-
 PROGRAM unif_sobol_points
       USE  sobol
       IMPLICIT NONE
@@ -17,40 +7,44 @@ PROGRAM unif_sobol_points
       REAL :: end_val, start_val, int_range, integral
       INTEGER :: i
       REAL*8 :: f
-
-
-      m = 1                                         !spatial dimension
-      n = 100                                       !number of sobol points generated
-      skip = 100                                    !starting sobol point
-      end_val = 10.0                                !Integral bounds
+!===============================Paramaters to be set===================================!
+!=============m: spatial dimension, n: Sobol Points, skip: point seed==================!
+!==============================val: integral bounds====================================!
+!======================================================================================!
+      m = 1                                         
+      n = 100                                      
+      skip = 100                                  
+      end_val = 10.0                             
       start_val = 0.0
-      int_range = end_val - start_val               !range of integral to normalize MC
-      f = 0.0D0                                     ! initialize values for integral
+      int_range = end_val - start_val           
+!======================================================================================!
+!================================initialize integral===================================!
+!======================================================================================!
+      f = 0.0D0                                
       integral = 0.0
-
       ALLOCATE (r(m,n))       
       ALLOCATE (q(m,n))       
-
-!Call the module to generate a uniform sobol sequence
+!======================================================================================!
+!==================================Generate points=====================================!
+!======================================================================================!
       CALL i8_sobol_generate(m, n, skip, r) 
-
-! sobol numbers are [0,1] we need to sample the whole function range
+!======================================================================================!
+!=====================sobol points: [0,1] need to sample fn domain=====================!
+!======================================================================================!
       q = r*int_range    
-    
-! for each value within q(m,n) call function and add up values
       DO i=1,n
           f = f+integrand(q(m,i))
       END DO
-
-! normalize your MC summation
+!======================================================================================!
+!=============================Normalize MC Integration=================================!
+!======================================================================================!
       f = f/n
       integral = (end_val-start_val)*f
-
-! Here is the numerical approximation of the integral!
-      WRITE(*,*) 'The numerical Integral is'
+      WRITE(*,*) 'The Numerical Integral Yields: '
       WRITE(*,*) integral
-        
-! Function that we are trying to integrate
+!======================================================================================!
+!===============================Function to Integrate==================================!
+!======================================================================================!
 CONTAINS
 
       FUNCTION integrand(r) RESULT (values)
@@ -61,5 +55,3 @@ CONTAINS
       END FUNCTION integrand
 
 END PROGRAM unif_sobol_points
-
-!This code is working properly 3/8/17 -Shane
