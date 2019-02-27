b8b9ed2481786f2a7ff55f4b0b19d0d332370bf9
swflynn <swflynn@uci.edu>
2019-02-26 19:43:57 -0800

    Project Main

--- Q_DGB/development/gaus_grid/run.sh
+++
@@ -1,2 +0,0 @@
-python g_grid.py -10 10 21 > out.dat
-xmgrace out.dat
--- Q_DGB/development/rejection/run.sh
+++
@@ -1,5 +0,0 @@
-./a.out < input > out
-plot.py out x y out.png
-plot.py all_data.dat x y all.png
-display out.png &
-display all.png &
